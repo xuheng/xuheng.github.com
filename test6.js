32ea7831af2230351b8614fa2f12e18f334db258
xuheng <312338568@qq.com>
2014-08-22 11:12:52 +0800

    by xuheng

---
+++ test6.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Edwan on 14-8-22.
+ */
+var demo = (function(){
+    var a , b,c;
+    var d = function(){};
+
+    return {
+        a1:a,
+        d2:d,
+        d3:function(){
+
+
+        }
+
+    }
+
+
+})()
